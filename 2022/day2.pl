4d4190e0f11a61f684adfe48e24cb6d6be0bcb65
isabel <isabel.wingen@innoq.com>
2022-12-03 19:33:39 +0100

    Solve Day2

---
+++ 2022/day2.pl
@@ -0,0 +1,69 @@
+% Facts
+result([rock,rock],draw).
+result([paper,paper],draw).
+result([scissors,scissors],draw).
+result([rock,scissors],lost).
+result([scissors,paper],lost).
+result([paper,rock],lost).
+result([rock,paper],won).
+result([paper,scissors],won).
+result([scissors,rock],won).
+
+points(rock,1).
+points(paper,2).
+points(scissors,3).
+points(won,6).
+points(draw,3).
+points(lost,0).
+
+matching1("A",rock).
+matching1("B",paper).
+matching1("C",scissors).
+matching1("X",rock).
+matching1("Y",paper).
+matching1("Z",scissors).
+
+matching2("X",lost).
+matching2("Y",draw).
+matching2("Z",won).
+
+% Read Input
+drop_last([_], []).
+drop_last([H|T], [H|S]) :-
+    drop_last(T,S).
+
+split_pair(X,Y) :- split_string(X," ","",Y).
+
+read_input(P) :-
+    read_file_to_string("resources/day2.txt",S,[]),
+    split_string(S,"\n","",L),
+    maplist(split_pair,L,Q),
+    drop_last(Q,P).
+
+% Part 1
+roundInPart1([ABC,XYZ],Score) :-
+    matching1(ABC,Opponent),
+    matching1(XYZ,Me),
+    result([Opponent,Me],Result),
+    points(Result,P2),
+    points(Me, P1),
+    Score is P1 + P2.
+
+solve_part_1(S) :-
+    read_input(Input),
+    maplist(roundInPart1,Input,Scores),
+    sumlist(Scores,S).
+
+% Part 2
+roundInPart2([ABC,XYZ],Score) :-
+    matching1(ABC,Opponent),
+    matching2(XYZ,Result),
+    result([Opponent,MyDraw],Result),
+    points(Result,P2),
+    points(MyDraw, P1),
+    Score is P1 + P2.
+
+solve_part_2(S) :-
+    read_input(Input),
+    maplist(roundInPart2,Input,Scores),
+    sumlist(Scores,S).

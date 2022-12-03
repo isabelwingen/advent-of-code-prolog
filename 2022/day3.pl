28306fc9b89000f5546c758644fb472b2f1bc5bc
isabel <isabel.wingen@innoq.com>
2022-12-03 19:33:54 +0100

    Solve Day3

---
+++ 2022/day3.pl
@@ -0,0 +1,48 @@
+% Read Input
+drop_last([_], []).
+drop_last([H|T], [H|S]) :-
+    drop_last(T,S).
+
+read_input(Rucksacks) :-
+    read_file_to_string("resources/day3.txt",S,[]),
+    split_string(S,"\n","",L),
+    drop_last(L,Strings),
+    maplist(string_chars,Strings,Rucksacks).
+
+find_element_in_boths(Rucksack, InBoth) :-
+    append(A, B, Rucksack),
+    length(A, N),
+    length(B, N),
+    member(InBoth, A),
+    member(InBoth, B), !.
+
+priority(Char, Prio) :-
+    char_code(Char, Code),
+    Code < 91, !,
+    Prio is Code - 38.
+
+priority(Char, Prio) :-
+    char_code(Char, Code),
+    Prio is Code - 96.
+
+solve_part_1(Result) :-
+    read_input(Rucksacks),
+    maplist(find_element_in_boths,Rucksacks,InBoth),
+    maplist(priority,InBoth,Prios),
+    sumlist(Prios,Result).
+
+split_in_groups_of_three([], []).
+split_in_groups_of_three([A,B,C|T], [[A,B,C]|S]) :-
+    split_in_groups_of_three(T,S).
+
+find_priority_of_badge([A,B,C],BadgePrio) :-
+    member(Badge,A),
+    member(Badge,B),
+    member(Badge,C),
+    priority(Badge,BadgePrio), !.
+
+solve_part_2(Result) :-
+    read_input(Rucksacks),
+    split_in_groups_of_three(Rucksacks, Groups),
+    maplist(find_priority_of_badge,Groups,BadgePrios),
+    sumlist(BadgePrios,Result).
